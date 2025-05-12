bc55b1c2f3b9b4d7e44173b31f3d09df6277a7b8
MinhKhoiDev24K <minhkhoi060224@gmail.com>
2025-05-12 17:32:35 +0700

    build

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -65,7 +65,7 @@ const Menu = ({ history }) => {
             <li className="nav-item">
               <Link to="/dashboard" className="nav-link">
                 <i className="nav-icon fas fa-tachometer-alt" />{" "}
-                <p> Dashboard v2.1 </p>
+                <p> Dashboard v3.0 </p>
               </Link>
             </li>
 
